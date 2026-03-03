0cacd561247bb1ece1a5af29c01304e47ce783b7
Rob Christie <robachristie@gmail.com>
2026-03-03 20:37:14 +1030

    mods to markdown handling

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,7 +1,12 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+   'MeanderingProgrammer/render-markdown.nvim',
+   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+   ---@module 'render-markdown'
+   ---@type render.md.UserConfig
+   opts = {
+      anti_conceal = {
+         -- This enables hiding added text on the line the cursor is on.
+         enabled = false,
+      },
+   },
 }
